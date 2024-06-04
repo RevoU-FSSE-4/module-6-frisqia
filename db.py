368c9ab922a6b43bc8365817cb8f849dea3fdd41
frisqia fatmalaningsih <frifani123@gmail.com>
2024-06-05 06:15:32 +0700

    db

---
+++ db.py
@@ -0,0 +1,23 @@
+animal = [
+    {   
+        "id" : 1,
+        "species": "Lion",
+        "age": 5,
+        "gender": "Male",
+        "special_requirements": "Requires large enclosure"
+},
+   {   
+        "id" : 2,
+        "species": "snake",
+        "age": 12,
+        "gender": "Male",
+        "special_requirements": "Requires large enclosure"
+},
+   {   
+        "id" : 3,
+        "species": "girrafe",
+        "age": 5,
+        "gender": "Female",
+        "special_requirements": "Requires large enclosure"
+},
+]
