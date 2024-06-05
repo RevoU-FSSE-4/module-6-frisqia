a4dfb1dc69337d84c10e27a92792d7b277a70b2e
frisqia fatmalaningsih <frifani123@gmail.com>
2024-06-05 20:26:37 +0700

    database

--- db.py
+++ db.py
@@ -1,23 +1,69 @@
+zoo = [
+        {
+        "id":1,
+        "species" : "Mamalia",
+        "special_requirements": "Requires large enclosure"
+        } ,      
+         {
+        "id":2,
+        "species" : "Aves",
+        "special_requirements": "Requires large enclosure"
+        } ,  
+        {
+        "id":3,
+        "species" : "Reptil",
+        "special_requirements": "Requires large enclosure"
+        } ,  
+]
+
 animal = [
-    {   
+          {   
         "id" : 1,
-        "species": "Lion",
-        "age": 5,
-        "gender": "Male",
-        "special_requirements": "Requires large enclosure"
-},
-   {   
+        "name": "Lion",
+        "zoo_id" : 1,
+        "food": "Karnivora",
+        "origin" :"Benua Afrika",
+        },
+         {
         "id" : 2,
-        "species": "snake",
-        "age": 12,
-        "gender": "Male",
-        "special_requirements": "Requires large enclosure"
-},
-   {   
+        "name": "Cendrawasih",
+        "zoo_id" : 2,
+        "food" : "Herbivora",
+        "origin" :"Jayapura",
+        },
+         {   
         "id" : 3,
-        "species": "girrafe",
-        "age": 5,
-        "gender": "Female",
-        "special_requirements": "Requires large enclosure"
-},
+        "name": "Estuarine Crocodile",
+        "zoo_id" : 3,
+        "food" : "Karnivora",
+        "origin" :"Australia",
+        },
+        {   
+        "id" : 4,
+        "name": "comodo",
+        "zoo_id" : 3,
+        "food" : "Karnivora",
+        "origin" :"Nusa Tenggara Timur, Indonesia",
+        },
+         {   
+        "id" : 5,
+        "name": "Pelican",
+        "zoo_id" : 2,
+        "food" : "Karnivora",
+        "origin" :"Amerika Selatan",
+        },
+             {   
+        "id" : 6,
+        "name": "hippopotamus",
+        "zoo_id" : 2,
+        "food" : "omnivora",
+        "origin" :"Afrika sub-Sahara",
+        },
+         {   
+        "id" : 7,
+        "name": "Gorila",
+        "zoo_id" : 1,
+        "food": "herbivora",
+        "origin" :"Afrika",
+        },
 ]
